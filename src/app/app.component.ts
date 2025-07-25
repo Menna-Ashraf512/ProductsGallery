4b9b26c20b38b005829eb93bea78f921d0805576
Menna Ashraf <mennaalla1512@gmail.com>
2025-07-25 18:55:03 +0300

    - Added @angular/animations, @fortawesome/fontawesome-free, flowbite, and ngx-toastr to package.json.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProductsGallery';
+  constructor(private toastrService:ToastrService){}
+  Toaster(){
+    console.log('toaster is working')
+    this.toastrService.success('okay','Done')
+  }
 }
